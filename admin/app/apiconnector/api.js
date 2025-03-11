7096b2ec99c1a38d0e0e1925b5e9ff3bfc296445
Sanskar Sisodia <110807171+Sanskar-Sisodia@users.noreply.github.com>
2025-03-11 11:38:07 +0530

    Update api.js

--- admin/app/apiconnector/api.js
+++ admin/app/apiconnector/api.js
@@ -1,6 +1,7 @@
 //const BASE_URL = 'http://52.66.8.192:8080/api';
 // const BASE_URL = 'http://localhost:8080/api';
-const BASE_URL  = "https://fil-x-connect-final-backend.onrender.com/api";
+//const BASE_URL  = "https://fil-x-connect-final-backend.onrender.com/api";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export async function apiRequest(endPoint, method, body) {
   const url = `${BASE_URL}/${endPoint}`;
   const response = await fetch(url, {
